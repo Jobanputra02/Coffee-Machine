5f3bca1544d37706762e2dcccf2bbf6692ab23f2
Chaitanya Jobanputra <60739703+Jobanputra02@users.noreply.github.com>
2022-05-18 11:18:10 +0530

    Add files via upload

---
+++ Coffee_Machine.py
@@ -0,0 +1,22 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+while is_on:
+    options = menu.get_items()
+    choice = input("\nEspresso($1.5)\nLatte($2.5)\nCappuccino($3)\nWhat would you like? ").lower()
+    if choice == "off":
+        print("Goodbye!")
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
